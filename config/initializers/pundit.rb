1026c45884552925201ebbc677a8525fe03fcdcc
fontain richardson <fontainwr@gmail.com>
2023-05-23 14:28:15 +0900

    Done

--- config/initializers/pundit.rb
+++ config/initializers/pundit.rb
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
 # Pundit.policy_ancestors = ['chomp']
-=======
-# # Pundit.policy_ancestors = ['ApplicationPolicy']
->>>>>>> master
 
 # Pundit.configure do |config|
 #   # Add your configuration here
