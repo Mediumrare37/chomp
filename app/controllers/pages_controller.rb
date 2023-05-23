d3f2cbd5c1b2d874a75e207f415e2b1c864cdf0b
Lucca Teruya <supersecureemailaccount@protonmail.com>
2023-05-23 12:39:22 +0900

    Created general.scss for general styles, and modified pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @task = Task.new
+    raise
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
   end
 end
