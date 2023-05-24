c9b7975b6c3534ce766a8da4fc8c421cc646d1e2
Lucca Teruya <supersecureemailaccount@protonmail.com>
2023-05-24 10:41:20 +0900

    Created card component, added card component to application.scss

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
 
-  # def index
-  #   @task = Task.all
-  # end
+  def index
+    @task = Task.all
+  end
 
   def show
     @task = Task.find(params[:id])
