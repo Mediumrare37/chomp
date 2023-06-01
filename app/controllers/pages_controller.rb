5984c8b38184c29da303c086a2763a4e8e8921c1
fontain richardson <fontainwr@gmail.com>
2023-06-01 14:12:54 +0900

    Removed line notifications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,9 @@ class PagesController < ApplicationController
 
   def home
     @task = Task.new
-    linebot = LineService.new(ENV["LINE_ID"])
-    message_text = "Task created"
-    linebot.send_message(message_text)
+    # linebot = LineService.new(ENV["LINE_ID"])
+    # message_text = "Welcome to Task Chomp"
+    # linebot.send_message(message_text)
   end
 
 
