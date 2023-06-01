5b794825ee18519e19f60ce0d8abb3f8b759ef27
fontain richardson <fontainwr@gmail.com>
2023-06-01 09:12:42 +0900

    finished LINE notifications

--- app/services/line_service.rb
+++ app/services/line_service.rb
@@ -6,36 +6,19 @@ class LineService
     @id = id
   end
 
-  # def client
-  #   p ENV['LINE_SECRET'], ENV['LINE_TOKEN']
-  #   p @client ||= Line::Bot::Client.new { |config|
-  #     config.channel_secret = ENV['LINE_SECRET']
-  #     config.channel_token = ENV['LINE_TOKEN']
-  #   }
-  # end
+  def client
+    p ENV['LINE_SECRET'], ENV['LINE_TOKEN']
+    p @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['LINE_SECRET']
+      config.channel_token = ENV['LINE_TOKEN']
+    }
+  end
 
   def send_message(text)
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_SECRET"]
-      config.channel_token = ENV["LINE_TOKEN"]
-    }
-
-    text = "plz fking work"
-
     message = {
       type: 'text',
       text: text
     }
-    p @client.push_message("55327ce05d1c61796906cac4b3573aa3", message)
-    # message = {
-    #   type: 'text',
-    #   text: text
-    # }
-    # p message
-    # p @id
-
-    # p client.push_message(@id, message)
+    client.push_message(@id, message)
   end
-
-
 end
